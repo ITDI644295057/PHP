5e5d9524b6035b618ef36a2ad74d2c724a9150ab
ITDI644295057 <99870577+ITDI644295057@users.noreply.github.com>
2023-04-18 12:48:52 +0700

    Update connect.php

--- 22032566/connect.php
+++ 22032566/connect.php
@@ -5,14 +5,15 @@
     $dbname = "shop";
 
     // Create connection เชื่อมต่อกับฐานข้อมูล
+    //MySQLi Procedural
     //$conn = mysqli_connect($servername, $dbusername, $dbpassword,$dbname);
-     // Check connection
+    // Check connection
     /*if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
         //echo "<center>Connected successfully</center>";//เชื่อมต่อได้*/
 
-    //เชิง OOP
+    //MySQLi Object-Oriented
     $conn = new mysqli($servername, $dbusername,$dbpassword,$dbname);
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
